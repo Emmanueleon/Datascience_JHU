6a14985c95805048486ff5a7bee33668101d7e58
Emmanueleon <emmanuel.cervam@gmail.com>
2023-04-19 17:25:43 -0600

    Histogram of Global Active Power script.

---
+++ C4.Exploratory_data_analysis/bin/plot1.R
@@ -0,0 +1,55 @@
+## Project 1: Histogram of Global Active Power
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# 1. Enviroment preparation   
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+## Limpiar el ambiente
+rm(list=ls())
+
+## Load libraries
+library(tidyverse)
+library(lubridate)
+
+## Import database
+epc <- read_delim("../data/household_power_consumption.txt",  delim= ";", )
+head(epc)
+
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# 2. Clean the data   
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+## Merge Date and Time and also parse variables "date" data type 
+epc <- epc%>%
+          unite(col = "complete_date", Date, Time, sep =" ", remove = FALSE)%>%
+          mutate(Date=dmy(Date),
+                 Time=hms(Time), 
+                 complete_date=dmy_hms(complete_date))%>%
+          filter(between(Date, as.Date("2007-02-01"),as.Date("2007-02-02")))
+          
+# We observe that numeric columns are typifyed as "character" data type due "?" symbol. 
+epc%>%
+          filter(Global_active_power=="?")
+
+## Clean the data
+### Make a vector with col names
+epc_names <- colnames(epc)
+
+### Remove Date and time columns
+epc_names <- epc_names%>%
+          setdiff(.,c("Date","Time", "complete_date"))
+
+### Remove "?" and transform to "numeric" data type
+epc <- epc%>%
+          mutate_at(vars(epc_names),~ str_remove(.,"\\?"))%>%
+          mutate_at(vars(epc_names),list(as.numeric))%>%
+          print()
+
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# 3. Graph   
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+png(filename = "../plot1.png", width = 480, height = 480, units = "px")
+with(epc, hist(Global_active_power, col="red", 
+               main="Global Active Power",
+               xlab="Global Active Power (kilowatts)"))
+dev.off()
